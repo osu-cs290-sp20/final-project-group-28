5186e4e083553591dff18d31d04c73c53cdb1ec1
Travis Coppinger <coppintr@oregonstate.edu>
2020-06-05 19:35:29 -0700

    Small formatting tweaks and server.js fix.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
-var port = process.env.PORT || 3322;
-var petData = require('./petData');     //from petData.json
+var port = process.env.PORT || 5522;
+var petData = require('./petInfo');     //from petData.json
 
 app.engine('handlebars', exphbs({defaultLayout: 'null' }));
 app.set('view engine', 'handlebars');
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 //home page
 app.get('/', function(req, res){
     res.status(200).render('homePage', {
-        pets: petData
+        //stuff?
     });
 });
 
@@ -24,7 +24,7 @@ app.get('/', function(req, res){
 //pets page
 app.get('/pets', function(req, res){
     res.status(200).render('petPage', {
-        //stuff?
+        pets: petData
     });
 });
 

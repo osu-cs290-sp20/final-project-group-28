bf7e09143e35d36f4cd0bce2e57eb902e288c81a
anthonyfil <56458147+anthonyfil@users.noreply.github.com>
2020-06-09 21:42:02 -0700

    corrected petInfo
    had two pets with the same name, fixed it.

--- public/petIndivTemplate.js
+++ public/petIndivTemplate.js
@@ -8,18 +8,18 @@ templates['petIndivTemplate'] = template({"compiler":[8,">= 4.3.0"],"main":funct
         return undefined
     };
 
-  return "<div class=\"pet-name\">\n    <h2>"
+  return "<div class=\"pet-name\">\r\n    <h2>"
     + alias4(((helper = (helper = lookupProperty(helpers,"petName") || (depth0 != null ? lookupProperty(depth0,"petName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petName","hash":{},"data":data,"loc":{"start":{"line":2,"column":8},"end":{"line":2,"column":19}}}) : helper)))
-    + "</h2>\n</div>\n\n<article class=\"pet-profile\">\n    <div class=\"profile-image\">\n        <img src="
+    + "</h2>\r\n</div>\r\n\r\n<article class=\"pet-profile\">\r\n    <div class=\"profile-image\">\r\n        <img src="
     + alias4(((helper = (helper = lookupProperty(helpers,"profilePic") || (depth0 != null ? lookupProperty(depth0,"profilePic") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profilePic","hash":{},"data":data,"loc":{"start":{"line":7,"column":17},"end":{"line":7,"column":31}}}) : helper)))
-    + " alt=\"Picture of small dog\" class=\"pet-image\"/>\n    </div>\n\n    <div class=\"pet-info\">\n        <p class=\"info-title\">\n            Species: \n        </p>\n\n        <p class=\"info-text\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"species") || (depth0 != null ? lookupProperty(depth0,"species") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"species","hash":{},"data":data,"loc":{"start":{"line":16,"column":12},"end":{"line":16,"column":23}}}) : helper)))
-    + "\n        </p>\n\n    </div>\n\n    <div class=\"pet-info\">\n        <p class=\"info-title\">\n            Breed: \n        </p>\n\n        <p class=\"info-text\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"breed") || (depth0 != null ? lookupProperty(depth0,"breed") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"breed","hash":{},"data":data,"loc":{"start":{"line":27,"column":12},"end":{"line":27,"column":21}}}) : helper)))
-    + "\n        </p>\n\n    </div>\n\n    <div class=\"pet-info\">\n        <p class=\"info-title\">\n            Favorite Toy:\n        </p>\n\n        <p class=\"info-text\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"favToy") || (depth0 != null ? lookupProperty(depth0,"favToy") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"favToy","hash":{},"data":data,"loc":{"start":{"line":38,"column":12},"end":{"line":38,"column":22}}}) : helper)))
-    + "\n        </p>\n\n    </div>\n\n    <div class=\"pet-info\">\n        <p class=\"info-title\">\n            Bio: \n        </p>\n\n        <p class=\"info-text\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"bio") || (depth0 != null ? lookupProperty(depth0,"bio") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"bio","hash":{},"data":data,"loc":{"start":{"line":49,"column":12},"end":{"line":49,"column":19}}}) : helper)))
-    + "\n        </p>\n\n    </div>\n\n</article>\n";
+    + " alt=\"Picture of small dog\" class=\"pet-image\"/>\r\n    </div>\r\n\r\n  <div class=\"pet-info-wrapper\">\r\n    <div class=\"pet-info\">\r\n        <p class=\"info-title\">\r\n            Species: \r\n        </p>\r\n\r\n        <p class=\"info-text\">\r\n            "
+    + alias4(((helper = (helper = lookupProperty(helpers,"species") || (depth0 != null ? lookupProperty(depth0,"species") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"species","hash":{},"data":data,"loc":{"start":{"line":17,"column":12},"end":{"line":17,"column":23}}}) : helper)))
+    + "\r\n        </p>\r\n\r\n    </div>\r\n\r\n    <div class=\"pet-info\">\r\n        <p class=\"info-title\">\r\n            Breed: \r\n        </p>\r\n\r\n        <p class=\"info-text\">\r\n            "
+    + alias4(((helper = (helper = lookupProperty(helpers,"breed") || (depth0 != null ? lookupProperty(depth0,"breed") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"breed","hash":{},"data":data,"loc":{"start":{"line":28,"column":12},"end":{"line":28,"column":21}}}) : helper)))
+    + "\r\n        </p>\r\n\r\n    </div>\r\n\r\n    <div class=\"pet-info\">\r\n        <p class=\"info-title\">\r\n            Favorite Toy:\r\n        </p>\r\n\r\n        <p class=\"info-text\">\r\n            "
+    + alias4(((helper = (helper = lookupProperty(helpers,"favToy") || (depth0 != null ? lookupProperty(depth0,"favToy") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"favToy","hash":{},"data":data,"loc":{"start":{"line":39,"column":12},"end":{"line":39,"column":22}}}) : helper)))
+    + "\r\n        </p>\r\n\r\n    </div>\r\n    </div>\r\n    <div class=\"pet-info-wrapper\">\r\n    <div class=\"pet-info\">\r\n        <p class=\"info-title\">\r\n            Bio: \r\n        </p>\r\n\r\n        <p class=\"info-text\">\r\n            "
+    + alias4(((helper = (helper = lookupProperty(helpers,"bio") || (depth0 != null ? lookupProperty(depth0,"bio") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"bio","hash":{},"data":data,"loc":{"start":{"line":51,"column":12},"end":{"line":51,"column":19}}}) : helper)))
+    + "\r\n        </p>\r\n\r\n    </div>\r\n    </div>\r\n\r\n</article>\r\n";
 },"useData":true});
 })();
